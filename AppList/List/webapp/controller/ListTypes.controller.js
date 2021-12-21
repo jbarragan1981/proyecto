3f4b44ee60a79efdd2a57283af85ed88bf3cf3c6
Johanna Barragan <johannabarragan@hotmail.com>
2021-12-21 20:08:50 +0000

    listados

---
+++ AppList/List/webapp/controller/ListTypes.controller.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("logaligroup.List.controller.ListTypes", {
+			onInit: function () {
+                var oJSONModel = new sap.ui.model.json.JSONModel();
+                oJSONModel.loadData("./localService/mockdata/ListData.json");
+                this.getView().setModel(oJSONModel);                
+			}
+		});
+	});
