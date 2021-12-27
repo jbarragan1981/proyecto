b8485b8e7203f9bea8a35c9da59762cd94309e0e
Johanna Barragan <johannabarragan@hotmail.com>
2021-12-27 04:33:13 +0000

    cambios_no_funciona

--- logali/employees/webapp/controller/Main.controller.js
+++ logali/employees/webapp/controller/Main.controller.js
@@ -9,15 +9,15 @@ sap.ui.define([
 
             // @ts-ignore
             var oJSONModelEmpl = new sap.ui.model.json.JSONModel();
-            oJSONModelEmpl.loadData("./localService/mockdata/Employees.json", false);
+            oJSONModelEmpl.loadData("../localService/mockdata/Employees.json", false);
             oView.setModel(oJSONModelEmpl, "jsonEmployees");
 
             var oJSONModelCountries = new sap.ui.model.json.JSONModel();
-            oJSONModelCountries.loadData("./localService/mockdata/Countries.json", false);
+            oJSONModelCountries.loadData("../localService/mockdata/Countries.json", false);
             oView.setModel(oJSONModelCountries, "jsonCountries");
 
             var oJSONModelLayout = new sap.ui.model.json.JSONModel();
-            oJSONModelLayout.loadData("./localService/mockdata/Layout.json", false);
+            oJSONModelLayout.loadData("../localService/mockdata/Layout.json", false);
             oView.setModel(oJSONModelLayout, "jsonLayout");
 
             var oJSONModelConfig = new sap.ui.model.json.JSONModel({
